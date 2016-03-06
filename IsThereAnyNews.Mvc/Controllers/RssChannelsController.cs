ed9f1649bd5b3f689b9b2514cb2d1db213045050
Jaroslaw Stadnicki <jaroslaw.stadnicki@gmail.com>
2016-03-06 11:24:13 +0100

    importing and displaying rss

---
+++ IsThereAnyNews.Mvc/Controllers/RssChannelsController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace IsThereAnyNews.Mvc.Controllers
+{
+    public class RssChannelsController : Controller
+    {
+        public ActionResult Index()
+        {
+            var rssChannelRepository = new RssChannelRepository();
+            var loadAllChannels = rssChannelRepository.LoadAllChannels();
+            var viewmodel = new RssChannelsIndexViewModel(loadAllChannels);
+            return this.View("Index", viewmodel);
+        }
+    }
+
+    public class RssChannelsIndexViewModel
+    {
+        public List<RssChannel> LoadAllChannels { get; set; }
+
+        public RssChannelsIndexViewModel(List<RssChannel> loadAllChannels)
+        {
+            LoadAllChannels = loadAllChannels;
+        }
+    }
+}
