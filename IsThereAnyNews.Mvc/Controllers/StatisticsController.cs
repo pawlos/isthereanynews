fd7df006a6fb5c1dadc825c4612cf0dbaab9352c
Jaroslaw Stadnicki <jaroslaw.stadnicki@gmail.com>
2016-04-03 16:20:14 +0200

    Placeholders

---
+++ IsThereAnyNews.Mvc/Controllers/StatisticsController.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+
+namespace IsThereAnyNews.Mvc.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return this.View("Index");
+        }
+
+        public ActionResult Channels()
+        {
+            return this.View("Channels");
+        }
+
+        public ActionResult News()
+        {
+            return this.View("News");
+        }
+
+        public ActionResult Users()
+        {
+            return this.View("Users");
+        }
+    }
+}
