690fd82074889bef3672edd8301f4c831c563634
Jaroslaw Stadnicki <jaroslaw.stadnicki@gmail.com>
2016-05-28 23:36:50 +0200

    mark all read

---
+++ IsThereAnyNews.Mvc/Scripts/itan/rssMarkChannelRead.js
@@ -0,0 +1,28 @@
+﻿(function ($) {
+    var onSuccessFormSubmit = function () {
+        $(".nocss-rsschannel-container .nocss-rss-item-list").html("");
+
+    };
+    var onErrorFormSubmit = function () { };
+
+    var onFormSubmit = function (e) {
+        e.preventDefault();
+        var element = $(e.currentTarget);
+
+        var ajaxOptions = {
+            url: element.attr("action"),
+            method: "POST",
+            data: element.serialize(),
+            success: onSuccessFormSubmit,
+            error: onErrorFormSubmit
+        }
+
+        $.ajax(ajaxOptions);
+    }
+
+    var onDocumentReady = function () {
+        $(".nocss-rsschannel-container").on("submit", ".nocss-mark-channel-read", onFormSubmit);
+    }
+
+    $(document).ready(onDocumentReady);
+})(jQuery);
