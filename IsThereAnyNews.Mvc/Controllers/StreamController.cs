dda17a01e1aba437959491537cf5510111a5e53d
Jaroslaw Stadnicki <jaroslaw.stadnicki@gmail.com>
2016-05-28 18:10:05 +0200

    marking as read when click on collapse

--- IsThereAnyNews.Mvc/Controllers/StreamController.cs
+++ IsThereAnyNews.Mvc/Controllers/StreamController.cs
@@ -1,10 +1,12 @@
-﻿using System.Web.Mvc;
+﻿using System.Net;
+using System.Web.Mvc;
 using IsThereAnyNews.Dtos;
 using IsThereAnyNews.Services;
 using IsThereAnyNews.SharedData;
 
 namespace IsThereAnyNews.Mvc.Controllers
 {
+    [Authorize]
     public class StreamController : BaseController
     {
         private readonly IRssSubscriptionService rssSubscriptionService;
@@ -18,17 +20,19 @@ namespace IsThereAnyNews.Mvc.Controllers
             this.rssSubscriptionService = rssSubscriptionService;
         }
 
+        [HttpGet]
         public ActionResult Read(StreamType streamType, long id, ShowReadEntries showReadEntries = ShowReadEntries.Hide)
         {
             var entries = this.rssSubscriptionService
                 .LoadAllUnreadRssEntriesToReadForCurrentUserFromSubscription(streamType, id, showReadEntries);
             return this.PartialView("_Read", entries);
         }
-      
+
+        [HttpPost]
         public ActionResult MarkRead(MarkReadDto dto)
         {
             this.rssSubscriptionService.MarkRead(dto);
-            return this.RedirectToAction("Read", new { StreamType = dto.StreamType, id = dto.Id });
+            return new HttpStatusCodeResult(HttpStatusCode.OK);
         }
     }
 }
