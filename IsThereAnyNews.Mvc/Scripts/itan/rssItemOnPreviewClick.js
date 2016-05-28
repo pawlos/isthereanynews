a670cc669b0837dda8a8d43b26deb9f70ad08953
Jaroslaw Stadnicki <jaroslaw.stadnicki@gmail.com>
2016-05-28 23:24:27 +0200

    mychannels and read stream starting to look alike

---
+++ IsThereAnyNews.Mvc/Scripts/itan/rssItemOnPreviewClick.js
@@ -0,0 +1,23 @@
+﻿(function($) {
+    var onSuccessShowPreviewText = function() {};
+    var onErrorShowPreviewText = function() {};
+
+    var onShowPreviewText = function(e) {
+        var element = $(e.currentTarget);
+
+        var ajaxOptions = {
+            url: element.data("url-mark-as-read"),
+            method: "POST",
+            success: onSuccessShowPreviewText,
+            error: onErrorShowPreviewText
+        }
+
+        $.ajax(ajaxOptions);
+    }
+
+    var onDocumentReady = function() {
+        $(".nocss-rsschannel-container").on("click", ".nocss-show-preview-text", onShowPreviewText);
+    }
+
+    $(document).ready(onDocumentReady);
+})(jQuery);
