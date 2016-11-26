ee9973e5cf02ec5721afe24c4c2d1274d715f210
Jaroslaw Stadnicki <jaroslaw.stadnicki@gmail.com>
2016-11-27 00:25:56 +0100

    droped session features

--- IsThereAnyNews.Services/Implementation/SessionProvider.cs
+++
@@ -1,8 +0,0 @@
-namespace IsThereAnyNews.Services.Implementation
-{
-    public class SessionProvider
-    {
-        private const string UserIdKey = "SessionProvider.Long.UserId";
-        private const string UserClaims = "SessionProvider.List.Claims";
-    }
-}
