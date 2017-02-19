1a9f3dc2c2415aef8964c83faee4df6d97293550
Jaroslaw Stadnicki <jaroslaw.stadnicki@gmail.com>
2017-02-19 15:05:57 +0100

    fixed missing this

--- IsThereAnyNews.Infrastructure.ConfigurationReader/Implementation/WebConfigReader.cs
+++ IsThereAnyNews.Infrastructure.ConfigurationReader/Implementation/WebConfigReader.cs
@@ -1,17 +1,16 @@
-﻿using System.Configuration;
-
-namespace IsThereAnyNews.Infrastructure.ConfigurationReader.Implementation
+﻿namespace IsThereAnyNews.Infrastructure.ConfigurationReader.Implementation
 {
+    using System.Configuration;
+
     public class WebConfigReader : IConfigurationReader
     {
-        public string FacebookAppId => GetStringValue("Facebook.AppId");
-        public string FacebookAppSecret => GetStringValue("Facebook.AppSecret");
-        public string TwitterConsumerKey => GetStringValue("Twitter.ConsumerKey");
-        public string TwitterConsumerSecret => GetStringValue("Twitter.ConsumerSecret");
-        public string GoogleClientId => GetStringValue("Google.ClientId");
-        public string GoogleClientSecret => GetStringValue("Google.ClientSecret");
+        public string FacebookAppId => this.GetStringValue("Facebook.AppId");
+        public string FacebookAppSecret => this.GetStringValue("Facebook.AppSecret");
+        public string TwitterConsumerKey => this.GetStringValue("Twitter.ConsumerKey");
+        public string TwitterConsumerSecret => this.GetStringValue("Twitter.ConsumerSecret");
+        public string GoogleClientId => this.GetStringValue("Google.ClientId");
+        public string GoogleClientSecret => this.GetStringValue("Google.ClientSecret");
 
         private string GetStringValue(string key) => ConfigurationManager.AppSettings[key];
     }
-
 }
